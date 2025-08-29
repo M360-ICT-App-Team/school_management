6ae1053334a74f4526a7cb96a60da0de2a25c77d
RifatHasan232 <rifat.m360ict@gmail.com>
2025-08-29 19:52:56 +0600

    zisan ar basay code

--- lib/features/teacher/attendance/presentation/pages/select_batch_teacher_page.dart
+++ lib/features/teacher/attendance/presentation/pages/select_batch_teacher_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:school_management/core/constants/app_sizes.dart';
 import 'package:school_management/core/widgets/app_empty.dart';
 import 'package:school_management/features/teacher/attendance/presentation/bloc/teacher_attendance_bloc.dart';
 
+import '../../../../../core/constants/app_text_styles.dart';
+import '../../../../../core/utilities/app_filtering_bottom_sheet.dart';
 import '../../../../../core/widgets/app_bar.dart';
+import '../../../../../core/widgets/app_bottom_list_sheet.dart';
+import '../../../../common/subject/data/model/subject_list_response_teacher_model.dart';
+import '../../../../common/subject/presentation/bloc/subject_bloc.dart';
 import '../widgets/select_batch_teacher_card_widget.dart';
 
 class SelectBatchTeacherPage extends StatefulWidget {
@@ -14,6 +20,9 @@ class SelectBatchTeacherPage extends StatefulWidget {
 }
 
 class _SelectBatchTeacherPageState extends State<SelectBatchTeacherPage> {
+  final ValueNotifier<SubjectListResponseTeacherModel?> selectedSubject =
+      ValueNotifier(null);
+
   @override
   void initState() {
     super.initState();
@@ -23,38 +32,151 @@ class _SelectBatchTeacherPageState extends State<SelectBatchTeacherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(),
-      body: BlocBuilder<TeacherAttendanceBloc, TeacherAttendanceState>(
-        buildWhen: (previous, current) =>
-            current is GetTeacherBatchOverViewLoading ||
-            current is GetTeacherBatchOverViewSuccess ||
-            current is GetTeacherBatchOverViewError,
+      appBar: const CustomAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: AppSizes.insidePadding),
+        child: Column(
+          children: [
+            //! for choosing subject
+            SizedBox(
+              width: 100,
+              child: InkWell(
+                onTap: () => pickSubject(context),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: AppSizes.insidePadding,
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color(0xffd9d9d9),
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.black, width: 1.5),
+                  ),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.arrow_drop_down,
+                        size: 20,
+                        color: Colors.black,
+                      ),
+                      const SizedBox(width: 6),
+                      Expanded(
+                        child:
+                            ValueListenableBuilder<
+                              SubjectListResponseTeacherModel?
+                            >(
+                              valueListenable: selectedSubject,
+                              builder: (context, value, child) {
+                                return Text(
+                                  value?.subjectName ?? "সাবজেক্ট",
+                                  overflow: TextOverflow.ellipsis,
+                                  style: AppTextStyles.normalLight(
+                                    context,
+                                  ).copyWith(fontSize: 14),
+                                );
+                              },
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
 
-        builder: (context, state) {
-          if (state is GetTeacherBatchOverViewLoading) {
-            return const Center(child: CircularProgressIndicator.adaptive());
-          } else if (state is GetTeacherBatchOverViewError) {
-            return Center(child: AppEmpty(msg: state.message));
-          } else if (state is GetTeacherBatchOverViewSuccess) {
-            if (state.batchOverViewTeacherResponseModel.isEmpty) {
-              return Center(child: AppEmpty(msg: "No batch found"));
-            }
-            return ListView.builder(
-              itemCount: state.batchOverViewTeacherResponseModel.length,
-              itemBuilder: (context, index) {
-                final batch = state.batchOverViewTeacherResponseModel[index];
-                return SelectBatchTeacherCardWidget(
-                  batchOverViewTeacherResponseModel: batch,
-                );
-              },
-            );
-          } else {
-            return Center(child: AppEmpty(msg: "Something went wrong"));
-          }
-        },
+            const SizedBox(height: 10),
+
+            //! for building batch list
+            Expanded(
+              child: BlocBuilder<TeacherAttendanceBloc, TeacherAttendanceState>(
+                buildWhen: (previous, current) =>
+                    current is GetTeacherBatchOverViewLoading ||
+                    current is GetTeacherBatchOverViewSuccess ||
+                    current is GetTeacherBatchOverViewError,
+                builder: (context, state) => buildBatchList(state),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  // Future<void> pickSubject(BuildContext context) async {
+  //   context.read<SubjectBloc>().add(GetSubjectEvent());
+  //   final selected =
+  //       await showSelectionBottomSheet<SubjectListResponseTeacherModel>(
+  //         context: context,
+  //         builder: (ctx, onSelect) {
+  //           return BlocBuilder<SubjectBloc, SubjectState>(
+  //             builder: (context, state) {
+  //               if (state is GetSubjectLoading) {
+  //                 return const Center(
+  //                   child: CircularProgressIndicator.adaptive(),
+  //                 );
+  //               } else if (state is GetSubjectSuccess) {
+  //                 if (state.subjectResponseModel.isEmpty) {
+  //                   return const Center(
+  //                     child: Text("কোনো সাবজেক্ট পাওয়া যায়নি"),
+  //                   );
+  //                 }
+  //                 return ListView.builder(
+  //                   itemCount: state.subjectResponseModel.length,
+  //                   itemBuilder: (context, index) {
+  //                     final subject = state.subjectResponseModel[index];
+  //                     return ListTile(
+  //                       title: Text(subject.subjectName ?? "unknown"),
+  //                       onTap: () => onSelect(subject),
+  //                     );
+  //                   },
+  //                 );
+  //               } else if (state is GetSubjectError) {
+  //                 return const Center(child: Text("কোনো সাবজেক্ট পাওয়া যায়নি"));
+  //               }
+  //               return const SizedBox.shrink();
+  //             },
+  //           );
+  //         },
+  //       );
+
+  //   if (selected != null) {
+  //     selectedSubject.value = selected;
+  //   }
+  // }
+
+  Future<void> pickSubject(BuildContext context) async {
+    final selected =
+        await filteringBottomSheet<SubjectListResponseTeacherModel>(
+          context: context,
+          bloc: context.read<SubjectBloc>(),
+          event: GetSubjectEvent(),
+          extractItems: (state) =>
+              state is GetSubjectSuccess ? state.subjectResponseModel : null,
+          getTitle: (s) => s.subjectName ?? "Unknown",
+          emptyText: "কোনো সাবজেক্ট পাওয়া যায়নি",
+        );
+
+    if (selected != null) selectedSubject.value = selected;
+  }
+
+  Widget buildBatchList(TeacherAttendanceState state) {
+    if (state is GetTeacherBatchOverViewLoading) {
+      return const Center(child: CircularProgressIndicator.adaptive());
+    } else if (state is GetTeacherBatchOverViewError) {
+      return Center(child: AppEmpty(msg: state.message));
+    } else if (state is GetTeacherBatchOverViewSuccess) {
+      if (state.batchOverViewTeacherResponseModel.isEmpty) {
+        return const Center(child: AppEmpty(msg: "No batch found"));
+      }
+      return ListView.builder(
+        itemCount: state.batchOverViewTeacherResponseModel.length,
+        itemBuilder: (context, index) {
+          final batch = state.batchOverViewTeacherResponseModel[index];
+          return SelectBatchTeacherCardWidget(
+            batchOverViewTeacherResponseModel: batch,
+          );
+        },
+      );
+    }
+    return const Center(child: AppEmpty(msg: "Something went wrong"));
+  }
 }
-
-
--- lib/features/teacher/attendance/presentation/widgets/filtering_attendance_widget.dart
+++ lib/features/teacher/attendance/presentation/widgets/filtering_attendance_widget.dart
@@ -4,10 +4,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../../core/constants/app_colors.dart';
 import '../../../../../core/constants/app_text_styles.dart';
 import '../../../../../core/utilities/app_convert_date_time.dart';
+import '../../../../../core/utilities/app_filtering_bottom_sheet.dart';
 import '../../../../../core/widgets/app_adaptive_date.dart';
-import '../../../../../core/widgets/app_bottom_list_sheet.dart';
-import '../../../../common/subject/data/model/subject_list_response_teacher_model.dart';
-import '../../../../common/subject/presentation/bloc/subject_bloc.dart';
 import '../../../branch/data/model/branch_response_model.dart';
 import '../../../branch/presentation/bloc/branch_bloc.dart';
 import '../../data/model/attendance_model.dart';
@@ -28,7 +26,9 @@ class _FilteringAttendanceWidgetState extends State<FilteringAttendanceWidget> {
   final ValueNotifier<String?> selectedSubject = ValueNotifier(null);
   final ValueNotifier<int?> selectedSubjectId = ValueNotifier(null);
 
-  final ValueNotifier<String?> selectedBranch = ValueNotifier(null);
+  final ValueNotifier<BranchResponseModel?> selectedBranch = ValueNotifier(
+    null,
+  );
   final ValueNotifier<int?> selectedBranchId = ValueNotifier(null);
   @override
   Widget build(BuildContext context) {
@@ -77,131 +77,9 @@ class _FilteringAttendanceWidgetState extends State<FilteringAttendanceWidget> {
           },
         ),
 
-        //! subject choice
-        InkWell(
-          onTap: () async {
-            context.read<SubjectBloc>().add(GetSubjectEvent());
-
-            final selected =
-                await showSelectionBottomSheet<SubjectListResponseTeacherModel>(
-                  context: context,
-                  builder: (ctx, onSelect) {
-                    return BlocBuilder<SubjectBloc, SubjectState>(
-                      builder: (context, state) {
-                        if (state is GetSubjectLoading) {
-                          return const Center(
-                            child: CircularProgressIndicator.adaptive(),
-                          );
-                        } else if (state is GetSubjectSuccess) {
-                          final subjects = state.subjectResponseModel;
-                          if (subjects.isEmpty) {
-                            return const Center(
-                              child: Text("কোনো সাবজেক্ট পাওয়া যায়নি"),
-                            );
-                          }
-
-                          return ListView.builder(
-                            itemCount: subjects.length,
-                            itemBuilder: (context, index) {
-                              final subject = subjects[index];
-                              return ListTile(
-                                title: Text(subject.subjectName ?? "unknown"),
-                                onTap: () => onSelect(subject),
-                              );
-                            },
-                          );
-                        } else if (state is GetSubjectError) {
-                          return const Center(
-                            child: Text("কোনো সাবজেক্ট পাওয়া যায়নি"),
-                          );
-                        }
-                        return const SizedBox.shrink();
-                      },
-                    );
-                  },
-                );
-
-            if (selected != null) {
-              selectedSubject.value = selected.subjectName;
-              selectedSubjectId.value = selected.subjectOfferingId;
-            }
-          },
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 6),
-            decoration: BoxDecoration(
-              color: const Color(0xffd9d9d9),
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(color: Colors.black, width: 1.5),
-            ),
-            child: Row(
-              children: [
-                const Icon(
-                  Icons.arrow_drop_down,
-                  size: 20,
-                  color: Colors.black,
-                ),
-                const SizedBox(width: 4),
-                ValueListenableBuilder<String?>(
-                  valueListenable: selectedSubject,
-                  builder: (context, value, child) {
-                    return Text(
-                      value ?? "সাবজেক্ট", // সিলেক্ট করা সাবজেক্ট দেখাবে
-                      style: AppTextStyles.normalLight(
-                        context,
-                      ).copyWith(fontSize: 14),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
-
         //!select branch
         InkWell(
-          onTap: () async {
-            context.read<BranchBloc>().add(GetBranchListEvent());
-
-            final selected =
-                await showSelectionBottomSheet<BranchResponseModel>(
-                  context: context,
-                  builder: (ctx, onSelect) {
-                    return BlocBuilder<BranchBloc, BranchState>(
-                      builder: (context, state) {
-                        if (state is GetBranchListLoading) {
-                          return const Center(
-                            child: CircularProgressIndicator.adaptive(),
-                          );
-                        } else if (state is GetBranchListSuccess) {
-                          final branches = state.branchResponseModel;
-                          return ListView.builder(
-                            itemCount: branches.length,
-                            itemBuilder: (context, index) {
-                              final branch = branches[index];
-                              return ListTile(
-                                title: Text(branch.name ?? "unknown"),
-                                onTap: () => onSelect(
-                                  branch,
-                                ), // <-- পুরো object পাঠাচ্ছি
-                              );
-                            },
-                          );
-                        } else if (state is GetBranchListError) {
-                          return const Center(
-                            child: Text("কোনো শাখা পাওয়া যায়নি"),
-                          );
-                        }
-                        return const SizedBox.shrink();
-                      },
-                    );
-                  },
-                );
-
-            if (selected != null) {
-              selectedBranch.value = selected.name;
-              selectedBranchId.value = selected.id;
-            }
-          },
+          onTap: () => pickBranch(context),
           child: Container(
             padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 6),
             decoration: BoxDecoration(
@@ -217,11 +95,11 @@ class _FilteringAttendanceWidgetState extends State<FilteringAttendanceWidget> {
                   color: Colors.black,
                 ),
                 const SizedBox(width: 4),
-                ValueListenableBuilder<String?>(
+                ValueListenableBuilder<BranchResponseModel?>(
                   valueListenable: selectedBranch,
                   builder: (context, value, child) {
                     return Text(
-                      value ?? "শাখা",
+                      value?.name ?? "শাখা",
                       style: AppTextStyles.normalLight(
                         context,
                       ).copyWith(fontSize: 14),
@@ -240,7 +118,7 @@ class _FilteringAttendanceWidgetState extends State<FilteringAttendanceWidget> {
               GetStudentListEvent(
                 id: widget.attendanceModel.batchId,
                 subjectId: widget.attendanceModel.subjectId,
-                branchId: selectedBranchId.value,
+                branchId: selectedBranch.value?.id,
                 date: attendanceDate.value,
               ),
             );
@@ -261,5 +139,19 @@ class _FilteringAttendanceWidgetState extends State<FilteringAttendanceWidget> {
         ),
       ],
     );
+  }
+
+  Future<void> pickBranch(BuildContext context) async {
+    final selected = await filteringBottomSheet<BranchResponseModel>(
+      context: context,
+      bloc: context.read<BranchBloc>(),
+      event: GetBranchListEvent(),
+      extractItems: (state) =>
+          state is GetBranchListSuccess ? state.branchResponseModel : null,
+      getTitle: (s) => s.name ?? "Unknown",
+      emptyText: "কোনো শাখা পাওয়া যায়নি",
+    );
+
+    if (selected != null) selectedBranch.value = selected;
   }
 }
