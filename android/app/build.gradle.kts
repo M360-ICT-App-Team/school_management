3f8b71cb389a71f185b12f55eee0236d60287a8b
sukanta-m360 <sukanta.m360ict@gmail.com>
2025-08-31 11:03:22 +0600

    upload play store 1.0.0+4 version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            //signingConfig = signingConfigs.getByName("release")
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+//            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
